297a21c8e9b6a8387c136dddefb29947511fb943
Shibin Hussain <67695017+shibinhussain@users.noreply.github.com>
2023-09-14 15:18:11 +0530

    Update testScriptOne.js

--- testScriptOne.js
+++ testScriptOne.js
@@ -1,39 +1,4 @@
-function testScript() {
-  console.log('TEST');
-  //   const styleElement = document.createElement('style');
 
-  //   styleElement.textContent = `
-  //   #consent-banner #truste-banner-logo {
-  //     height: auto !important;
-  //     padding: 40px 0;
-  //   }
-  // `;
-
-  //   document.head.appendChild(styleElement);
-
-  //   const div1 = document.createElement('div');
-  //   div1.id = 'teconsent';
-  //   div1.style.backgroundColor = '#636466';
-  //   div1.style.position = 'absolute';
-  //   div1.style.bottom = '0';
-  //   div1.style.height = '40px';
-  //   div1.style.width = '100%';
-  //   div1.style.textAlign = 'center';
-  //   div1.style.color = 'white';
-
-  //   const div2 = document.createElement('div');
-  //   div2.id = 'consent-banner';
-  //   div2.style.position = 'fixed';
-  //   div2.style.bottom = '0';
-  //   div2.style.zIndex = '1040';
-  //   div2.style.maxHeight = '100vh';
-  //   div2.style.maxWidth = '100%';
-  //   div2.style.overflow = 'auto';
-
-  //   document.body.appendChild(div1);
-  //   document.body.appendChild(div2);
-}
 
 console.log('2 test');
 
-testScript();
